d64298706e91dc6aee238ffbf7d7c6cd2657392f
Koji Sakamoto <kowwwwji@gmail.com>
2023-08-15 05:51:42 +0900

    fix: vim key bind

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -4,3 +4,16 @@
 
 vim.keymap.set("n", ";", ":")
 vim.keymap.set("n", ":", ";")
+
+-- move line
+-- Option + J/K
+-- ∆ == J ˚ == K
+vim.keymap.set("n", "˚", ":m .-2<CR>==", { silent = true })
+vim.keymap.set("n", "∆", ":m .+1<CR>==", { silent = true })
+vim.keymap.set("i", "˚<Esc>", ":m .-2<CR>==gi", { silent = true })
+vim.keymap.set("i", "∆<Esc>", ":m .+1<CR>==gi", { silent = true })
+vim.keymap.set("v", "˚", ":m '<-2<CR>gv=gv", { silent = true })
+vim.keymap.set("v", "∆", ":m '>+1<CR>gv=gv", { silent = true })
+
+-- 選択時に改行を含まない
+vim.keymap.set("v", "$", "g_", { silent = true })
