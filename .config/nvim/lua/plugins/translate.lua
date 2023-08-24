6c735426e20374710692eeed5216d02aaf4db7f0
k-sakamoto <k-sakamoto@onecompath.com>
2023-08-24 10:57:56 +0900

    fix: translate.lua

--- .config/nvim/lua/plugins/translate.lua
+++ .config/nvim/lua/plugins/translate.lua
@@ -1,14 +1,25 @@
 return {
   "skanehira/translate.vim",
+  keys = {
+    {
+      mode = { "n", "v" },
+      "<Leader>h",
+      ":Translate<CR>",
+      desc = "Translate English",
+    },
+    {
+      mode = { "n", "v" },
+      "<Leader>H",
+      ":Translate!<CR>",
+      desc = "Translate Japanese",
+    },
+  },
   opts = {
     translate_source = "en",
     translate_target = "ja",
     -- if you want use popup window, set value 1
-    translate_popup_window = 0,
+    translate_popup_window = 1,
     -- set buffer window height size if you doesn't use popup window
-    translate_winsize = 5,
-    vim.keymap.set({ "n", "v" }, "<Leader>h", ":Translate<CR>", { silent = true, desc = "Translate English" }),
-    vim.keymap.set({ "n", "v" }, "<Leader>H", ":Translate!<CR>", { silent = true, desc = "Translate Japanese" }),
-    vim.keymap.set({ "n" }, "<Esc><Esc><Esc>", ":bw translate://result<CR>", { silent = true }),
+    -- translate_winsize = 5,
   },
 }
