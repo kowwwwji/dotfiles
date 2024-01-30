e46afc4355f5eea2a51e1ddc7121af0ff966321e
k-sakamoto <k-sakamoto@onecompath.com>
2024-01-30 17:13:43 +0900

    delete: previm open-browser

--- .config/nvim/lua/plugins/browser.lua
+++ .config/nvim/lua/plugins/browser.lua
@@ -1,34 +1,5 @@
 return {
   {
-    "tyru/open-browser.vim",
-    event = "VimEnter",
-    keys = {
-      {
-        "<Leader>o",
-        function()
-          return "<Plug>(openbrowser-smart-search)"
-        end,
-        expr = true,
-        mode = { "n", "v" },
-        desc = "Search cursol/selection words in web",
-      },
-    },
-  },
-  {
-    "previm/previm",
-    ft = { "md", "markdown" },
-    lazy = true,
-    keys = {
-      {
-        "<Leader>p",
-        ":PrevimOpen<CR>",
-        mode = "n",
-        silent = true,
-        desc = "Preview markdown",
-      },
-    },
-  },
-  {
     "almo7aya/openingh.nvim",
     keys = {
       {
