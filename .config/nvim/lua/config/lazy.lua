cb86a3713b5d33702c3328107dda8e3be7a16735
k-sakamoto <k-sakamoto@onecompath.com>
2024-02-28 18:35:21 +0900

    add: nvim-dap

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -22,9 +22,11 @@ require("lazy").setup({
     -- { import = "lazyvim.plugins.extras.lang.typescript" },
 
     { import = "lazyvim.plugins.extras.dap.core" },
+    { import = "lazyvim.plugins.extras.dap.nlua" },
 
     -- INFO: Copilotを使用しない時はコメントアウトする
     { import = "lazyvim.plugins.extras.coding.copilot" },
+    { import = "lazyvim.plugins.extras.vscode" },
 
     { import = "plugins" },
   },
