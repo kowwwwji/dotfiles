09f0b4e465a21ff26b66b165e13e37b1c57d7a69
k-sakamoto <k-sakamoto@onecompath.com>
2024-03-26 18:13:21 +0900

    fix: exclude public dir

--- .config/nvim/lua/plugins/todo-comments.lua
+++ .config/nvim/lua/plugins/todo-comments.lua
@@ -5,6 +5,7 @@ return {
     search = {
       command = "rg",
       args = {
+        "--glob=!public/", -- exclude public directory
         "--color=never",
         "--no-heading",
         "--with-filename",
@@ -15,8 +16,8 @@ return {
       },
       -- regex that will be used to match keywords.
       -- don't replace the (KEYWORDS) placeholder
-      pattern = [[\b(KEYWORDS):]], -- ripgrep regex
-      -- pattern = [[\b(KEYWORDS)\b]], -- match without the extra colon. You'll likely get false positives
+      -- pattern = [[\b(KEYWORDS):]], -- ripgrep regex
+      pattern = [[\b(KEYWORDS)\b]], -- match without the extra colon. You'll likely get false positives
     },
   },
 }
