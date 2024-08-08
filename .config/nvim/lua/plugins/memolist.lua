e969ffd9b87ad0a7fd53082e6c3e5dea6d76f7fd
k-sakamoto <k-sakamoto@onecompath.com>
2024-08-08 23:33:17 +0900

    feat(nvim): add memolist

---
+++ .config/nvim/lua/plugins/memolist.lua
@@ -0,0 +1,34 @@
+return {
+  {
+    "glidenote/memolist.vim",
+    keys = {
+      { "<Leader>mn", ":MemoNew<CR>", mode = "n", { silent = true, noremap = true }, desc = "Memo New" },
+      { "<Leader>ml", ":Telescope memo list<CR>", mode = "n", { silent = true, noremap = true }, desc = "Memo List" },
+      {
+        "<Leader>mg",
+        ":Telescope memo live_grep<CR>",
+        mode = "n",
+        { silent = true, noremap = true },
+        desc = "Memo Grep",
+      },
+    },
+    config = function()
+      vim.g.memolist_memo_suffix = "md"
+      vim.g.memolist_fzf = 1
+      -- vim.g.memolist_path = vim.env.MEMO_DIR
+      vim.g.memolist_path = "~/memo"
+      vim.g.memolist_template_dir_path = "~/.config/memo/template.txt"
+    end,
+  },
+  {
+    {
+      "delphinus/telescope-memo.nvim",
+      requires = {
+        "nvim-telescope/telescope.nvim",
+      },
+      config = function()
+        require("telescope").load_extension("memo")
+      end,
+    },
+  },
+}
