d75c76b6091387ed552ccb7b184f6c23344a5932
Koji Sakamoto <kowwwwji@gmail.com>
2024-08-16 01:56:19 +0900

    fix(memo): change memo setting

--- .config/memo/template.md
+++ .config/memo/template.md
@@ -1,8 +1,7 @@
-title: {{_title_}}
-==========
+---
 date: {{_date_}}
 tags: [{{_tags_}}]
 categories: [{{_categories_}}]
-- - -
-
-#
+---
+<!-- markdownlint-disable MD041 MD022 -->
+# {{_title_}}
--- .config/nvim/lua/plugins/memolist.lua
+++ .config/nvim/lua/plugins/memolist.lua
@@ -2,22 +2,24 @@ return {
   {
     "glidenote/memolist.vim",
     keys = {
-      { "<Leader>mn", ":MemoNew<CR>", mode = "n", { silent = true, noremap = true }, desc = "Memo New" },
-      { "<Leader>ml", ":Telescope memo list<CR>", mode = "n", { silent = true, noremap = true }, desc = "Memo List" },
+      -- TODO: iconを設定したい
+      { "<Leader>m", mode = "n", desc = "Memo" },
+      { "<Leader>mn", ":MemoNew<CR>", mode = "n", { silent = true, noremap = true }, desc = "New memo" },
+      { "<Leader>ml", ":Telescope memo list<CR>", mode = "n", { silent = true, noremap = true }, desc = "Show List" },
       {
         "<Leader>mg",
         ":Telescope memo live_grep<CR>",
         mode = "n",
         { silent = true, noremap = true },
-        desc = "Memo Grep",
+        desc = "Grep",
       },
-      { "<Leader>mt", ":e ~/memo/todo.md<CR>", mode = "n", { silent = true, noremap = true }, desc = "Open ToDo.md" },
+      { "<Leader>mt", ":e ~/memo/todo.md<CR>", mode = "n", { silent = true, noremap = true }, desc = "Open todo.md" },
     },
     config = function()
       vim.g.memolist_memo_suffix = "md"
       vim.g.memolist_fzf = 1
       vim.g.memolist_path = "~/memo"
-      vim.g.memolist_template_dir_path = "~/.config/memo/template.txt"
+      vim.g.memolist_template_dir_path = "~/.config/memo/template.md"
     end,
   },
   {
