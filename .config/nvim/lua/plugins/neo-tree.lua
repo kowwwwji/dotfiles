361252f7416dffcfbfc498fd74590e20bcc265cf
Koji Sakamoto <kowwwwji@gmail.com>
2025-10-02 14:08:34 +0900

    fix(nvim) neo-tree

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -4,24 +4,19 @@ return {
     {
       "<leader>fe",
       function()
-        require("neo-tree.command").execute({ toggle = true, dir = vim.loop.cwd() })
+        require("neo-tree.command").execute({ toggle = true, dir = vim.uv.cwd() })
       end,
       desc = "Explorer NeoTree (root)",
     },
-    {
-      "<leader>fE",
-      function()
-        require("neo-tree.command").execute({ toggle = true, dir = require("lazyvim.util").get_root() })
-      end,
-      desc = "Explorer NeoTree (cwd)",
-    },
     { "<leader>e", "<leader>fe", desc = "Explorer NeoTree (root)", remap = true },
     { "<C-e>", "<leader>fe", desc = "Explorer NeoTree (root)", remap = true },
-    { "<leader>E", "<leader>fE", desc = "Explorer NeoTree (cwd)", remap = true },
   },
   opts = {
     close_if_last_window = true,
     filesystem = {
+      follow_current_file = {
+        enabled = true,
+      },
       filtered_items = {
         hide_dotfiles = false,
         hide_by_name = {
