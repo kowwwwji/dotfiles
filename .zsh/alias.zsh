a3b42002e00677b6fe5e004258d2fbff42bd3856
movus-koji-sakamoto <koji.sakamoto@mo-vus.com>
2026-01-09 13:43:25 +0900

    add(alias ) claude code

--- .zsh/alias.zsh
+++ .zsh/alias.zsh
@@ -35,3 +35,4 @@ alias npt='npm run test'
 alias gc='gcloud'
 alias mac-notify='terminal-notifier -title "iTerm2" -sound Glass -message $1 '
 alias gitroot='git rev-parse --show-toplevel'
+alias cl=claude
