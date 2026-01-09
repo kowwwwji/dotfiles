4ea124f2e8602bbef7bce19b840b8c48c54c98cc
movus-koji-sakamoto <koji.sakamoto@mo-vus.com>
2026-01-09 16:49:48 +0900

    fix(lsp) add sqlls

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -7,5 +7,22 @@ return {
   },
   {
     "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        sqlls = {
+          -- Mason で入れたバイナリが自動的にパス解決される
+          cmd = { "sql-language-server", "up", "--method", "stdio" },
+          filetypes = { "sql" },
+          root_dir = function(fname)
+            local util = require("lspconfig.util")
+            -- fname が文字列であることを保証
+            if type(fname) ~= "string" then
+              return vim.loop.cwd()
+            end
+            return util.find_git_ancestor(fname) or vim.loop.cwd()
+          end,
+        },
+      },
+    },
   },
 }
