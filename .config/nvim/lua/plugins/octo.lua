f9f859db88fda2081bef05f91397f30383ceb20a
movus-koji-sakamoto <koji.sakamoto@mo-vus.com>
2026-02-10 19:02:02 +0900

    add(nvim) git(octo) keymap

---
+++ .config/nvim/lua/plugins/octo.lua
@@ -0,0 +1,7 @@
+return {
+  "pwntester/octo.nvim",
+  keys = {
+    { "<leader>gp", "<cmd>Octo pr<CR>", desc = "Open current branch PR (Octo)" },
+    { "<leader>gP", "<cmd>Octo pr list<CR>", desc = "List PRs (Octo)" },
+  },
+}
