2b7dca27b10022bc5cabe3cb4de946f0077ba765
movus-koji-sakamoto <koji.sakamoto@mo-vus.com>
2026-02-13 13:09:59 +0900

    fix(nvim) add notification feedback when opening PR via Octo keymap

--- .config/nvim/lua/plugins/octo.lua
+++ .config/nvim/lua/plugins/octo.lua
@@ -1,7 +1,14 @@
 return {
   "pwntester/octo.nvim",
   keys = {
-    { "<leader>gp", "<cmd>Octo pr<CR>", desc = "Open current branch PR (Octo)" },
+    {
+      "<leader>gp",
+      function()
+        vim.notify("Opening PR...", vim.log.levels.INFO)
+        vim.cmd("Octo pr")
+      end,
+      desc = "Open current branch PR (Octo)",
+    },
     { "<leader>gP", "<cmd>Octo pr list<CR>", desc = "List PRs (Octo)" },
   },
 }
