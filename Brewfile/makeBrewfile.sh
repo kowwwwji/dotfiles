013f87192190a1a78f435b68f1b6634a661d07d6
Koji Sakamoto <kowwwwji@gmail.com>
2026-03-10 23:16:26 +0900

    fix(brew) can exec anywhere

--- Brewfile/makeBrewfile.sh
+++ Brewfile/makeBrewfile.sh
@@ -1,5 +1,14 @@
 #!/bin/zsh
 
-rm Brewfile;
-brew bundle dump;
-sort -u Brewfile -o Brewfile -r;
+# Get the directory where this script is located
+SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
+BREWFILE_PATH="$SCRIPT_DIR/Brewfile"
+
+# Remove existing Brewfile
+rm -f "$BREWFILE_PATH"
+
+# Generate new Brewfile
+brew bundle dump --file="$BREWFILE_PATH"
+
+# Sort the Brewfile
+sort -u "$BREWFILE_PATH" -o "$BREWFILE_PATH" -r
